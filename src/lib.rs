fdc554db4d20cac1baff6a8d577869ac840ca86e
荀涧林 <kalxd@126.com>
2022-10-26 21:02:20 +0800

    FC

---
+++ src/lib.rs
@@ -0,0 +1,53 @@
+use std::ffi::{c_char, CStr, CString};
+
+use scraper::{html::Select, ElementRef, Html, Selector};
+
+mod rule;
+
+#[no_mangle]
+pub extern "C" fn parse_html(content: *const c_char) -> *const Html {
+    let cstr = unsafe { CStr::from_ptr(content) };
+    let html = Html::parse_document(cstr.to_str().unwrap());
+
+    crate::raw_point!(html)
+}
+
+#[no_mangle]
+pub extern "C" fn parse_fragment(char_ptr: *const c_char) -> *const Html {
+    let cstr = unsafe { CStr::from_ptr(char_ptr) };
+    let html = Html::parse_fragment(cstr.to_str().unwrap());
+
+    crate::raw_point!(html)
+}
+
+#[no_mangle]
+pub extern "C" fn build_select(char_ptr: *const c_char) -> *const Selector {
+    let cstr = unsafe { CStr::from_ptr(char_ptr) };
+    raw_point!(Selector::parse(cstr.to_str().unwrap()).unwrap())
+}
+
+#[no_mangle]
+pub extern "C" fn select<'a, 'b>(
+    html: *const Html,
+    selector: *const Selector,
+) -> *mut Select<'a, 'b> {
+    let html = unsafe { html.as_ref().unwrap() };
+    let selector = unsafe { selector.as_ref().unwrap() };
+    raw_point!(html.select(&selector))
+}
+
+#[no_mangle]
+pub extern "C" fn next_select<'a, 'b>(select: *mut Select<'a, 'b>) -> *const ElementRef<'a> {
+    let select: &mut Select = unsafe { select.as_mut().unwrap() };
+
+    match dbg!(select.next()) {
+        Some(s) => raw_point!(s),
+        None => std::ptr::null(),
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn element_html<'a>(el: *const ElementRef<'a>) -> *const c_char {
+    let el = unsafe { el.as_ref().unwrap() };
+    CString::new(el.html()).unwrap().into_raw()
+}
