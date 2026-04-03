737f75c4530abcaae4b5c9459a8df0549c824024
荀洪道 <kalxd@126.com>
2026-04-03 20:04:39 +0800

    又添加一些可能有用的方法。

--- src/reqwest/url.rs
+++ src/reqwest/url.rs
@@ -1,5 +1,5 @@
 use reqwest::Url;
-use std::ffi::c_char;
+use std::ffi::{CString, c_char};
 
 use crate::{
 	into_result,
@@ -22,4 +22,25 @@ extern "C" fn url_free(url: *mut Url) {
 extern "C" fn url_parse(input: *const c_char) -> *const CResult<*const Url> {
 	let url = unsafe { unsafe_str(input) };
 	into_result!(Url::parse(url))
+}
+
+#[unsafe(no_mangle)]
+extern "C" fn url_has_host(host: *const c_char, url: *const Url) -> bool {
+	let url = unsafe { &*url };
+	let host = unsafe { unsafe_str(host) };
+
+	url.host_str() == Some(host)
+}
+
+#[unsafe(no_mangle)]
+extern "C" fn url_filename(url: *const Url) -> *const c_char {
+	let url = unsafe { &*url };
+
+	let filepath = url.to_file_path().ok();
+	let filepath = filepath.as_ref().and_then(|p| p.to_str());
+
+	match filepath {
+		Some(path) => CString::new(path).unwrap().into_raw(),
+		None => std::ptr::null(),
+	}
 }
