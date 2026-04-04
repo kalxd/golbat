8d2b30ac1e0c1a8c3fb0a6020a5d1f093861cb3b
荀洪道 <kalxd@126.com>
2026-04-04 18:21:13 +0800

    开始为url贺彩！

--- src/reqwest/url.rs
+++ src/reqwest/url.rs
@@ -2,7 +2,7 @@ use reqwest::Url;
 use std::ffi::{CString, c_char};
 
 use crate::{
-	into_result,
+	into_ptr, into_result,
 	result::CResult,
 	rule::{drop_ptr, unsafe_str},
 };
@@ -22,6 +22,25 @@ extern "C" fn url_free(url: *mut Url) {
 extern "C" fn url_parse(input: *const c_char) -> *const CResult<*const Url> {
 	let url = unsafe { unsafe_str(input) };
 	into_result!(Url::parse(url))
+}
+
+// # panic
+// 直接假定input就是合法url！
+#[unsafe(no_mangle)]
+extern "C" fn url_unsafe_parse(input: *const c_char) -> *const Url {
+	let url = unsafe { unsafe_str(input) };
+	let url = Url::parse(url).expect(&format!("{url}不是有效的URL！"));
+	into_ptr!(url)
+}
+
+// 不会改变原有url，set_path会生成一条新的url。
+#[unsafe(no_mangle)]
+extern "C" fn url_set_path(url: *const Url, path: *const c_char) -> *const Url {
+	let path = unsafe { unsafe_str(path) };
+	let mut url = unsafe { &*url }.clone();
+
+	url.set_path(path);
+	into_ptr!(url)
 }
 
 #[unsafe(no_mangle)]
